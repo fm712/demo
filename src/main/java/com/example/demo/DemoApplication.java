b33e8d5d3e447a446c47b6aa4d2e3e1e49b68aab
fm712 <fmohammed712@robertmorris.edu>
2018-07-23 14:09:50 -0500

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -17,6 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	//Spring Boot App
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
